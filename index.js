668f313495d4fd9fcf81adfe88b2f3aca6768d33
Mariam Tokarski <mariamtokarski@gmail.com>
2018-01-23 06:39:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Mariam"
-var height = "70"
+var height = 70
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-parseInt(console.log(height+1))
+(console.log(height+1)
